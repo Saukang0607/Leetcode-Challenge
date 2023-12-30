37618f3c82bdb5065628cddb22fdc7e9441efd07
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2023-12-30 15:35:01 +0800

    Time: 203 ms (8.53%) | Memory: 19.6 MB (8.03%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,8 +1,8 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         sum = 0
-        max_left = [0] * len(height)
-        max_right = [0] * len(height)
+        max_left = [0] * len(height) #the tallest wall at its left
+        max_right = [0] * len(height) #the tallest wall at its right
 
         for l in range(1,len(height)-1):
             max_left[l] = max(height[l-1],max_left[l-1])
@@ -13,6 +13,6 @@ class Solution:
         for i in range(1,len(height)-1):
             minimum = min(max_left[i],max_right[i])
             if minimum > height[i]:
-                sum += minimum-height[i]
+                sum += minimum-height[i] #calculating the volume of that column
         
         return sum
