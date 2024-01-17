2f67c2830725eeca6231cdf0e7af942a805e44e2
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-01-17 23:54:14 +0800

    Time: 38 ms (86.12%) | Memory: 17.5 MB (9.52%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,6 +1,6 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        count = Counter(arr)
+        count = Counter(arr) 
         check = []
         for x in count.values():
             if x not in check:
@@ -8,4 +8,6 @@ class Solution:
             else: 
                 return False
         return True
+
+#17/1/2024 11:53, struggling on this easy question and probably going to die in next day intern
         
