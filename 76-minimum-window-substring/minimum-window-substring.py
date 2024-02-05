c07a9c1dcd989c37a0e12c9a2c50e0eaf19d52e2
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-02-05 23:50:40 +0800

    Time: 95 ms (74.05%) | Memory: 17.5 MB (56.21%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -2,23 +2,21 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         need = Counter(t)
         needCnt = len(t)
+        res = (0, float('inf'))
         i = 0
-        res = (0,float('inf'))
-        for j, c in enumerate(s):
-            if need[c] > 0:
+        for j,x in enumerate(s):
+            if need[x] > 0:
                 needCnt -= 1
-            need[c] -= 1
-            if needCnt == 0:
-                while True:
-                    c = s[i]
-                    if need[c] == 0: #found a char that is in t
+            need[x] -= 1
+            if needCnt == 0: #step 1: found a valid window
+                while True: #step 2: increase i (shorten the window)
+                    if need[s[i]] == 0: #cannot throw anymore
                         break
-                    need[c] += 1
-                    i += 1
-                if j - i < res[1] - res[0]:
-                    res = (i, j)
-                need[s[i]] += 1
+                    need[s[i]] += 1
+                    i += 1 #throwing unwanted elements
+                if j-i < res[1]-res[0]: #recording minimum
+                    res = (i,j)
+                need[s[i]] += 1 #step 3: repeat step 1 to find another window
                 needCnt += 1
-                i+=1
-        return "" if res[1] > len(s) else s[res[0]: res[1]+ 1]
-            
+                i += 1
+        return '' if res[1] > len(s) else s[res[0]:res[1]+1]
