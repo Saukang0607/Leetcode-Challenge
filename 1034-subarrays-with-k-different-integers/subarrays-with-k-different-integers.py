ba63a0135368c0722d580a50c45abe1c50811f74
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-03-31 01:45:49 +0800

    Time: 693 ms (5.09%) | Memory: 19.9 MB (74.32%) - LeetSync

---
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def subarraysWithKDistinct(self, A, K):
+        """
+        :type A: List[int]
+        :type K: int
+        :rtype: int
+        """
+        return self.atMostK(A, K) - self.atMostK(A, K - 1)
+    
+    def atMostK(self, A, K):
+        N = len(A)
+        left, right = 0, 0
+        counter = collections.Counter()
+        distinct = 0
+        res = 0
+        while right < N:
+            if counter[A[right]] == 0:
+                distinct += 1
+            counter[A[right]] += 1
+            while distinct > K:
+                counter[A[left]] -= 1
+                if counter[A[left]] == 0:
+                    distinct -= 1
+                left += 1
+            res += right - left + 1
+            print(left, right, res)
+            right += 1
+        return res
