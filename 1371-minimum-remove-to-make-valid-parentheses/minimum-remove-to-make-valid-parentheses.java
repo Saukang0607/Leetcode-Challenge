2e0b1162051a0c41b4db9f067b181dfc204ae3a8
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-04-07 14:06:19 +0800

    Time: 25 ms (38.01%) | Memory: 45.4 MB (56.14%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Set<Integer> indexesToRemove = new HashSet<>();
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(') { stack.push(i); }
+            else if(s.charAt(i) == ')') {
+                if(stack.isEmpty()) { indexesToRemove.add(i); } //not valid
+                else { stack.pop(); }
+            }
+        }
+        while(!stack.isEmpty()) { indexesToRemove.add(stack.pop()); } //not valid
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            if(!indexesToRemove.contains(i)){
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+
+    }
+}
