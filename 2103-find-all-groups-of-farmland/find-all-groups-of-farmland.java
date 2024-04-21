25ea452fbc921634326b7da8a98ca255175ad62c
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-04-21 10:54:40 +0800

    Time: 3 ms (100.00%) | Memory: 59.4 MB (88.02%) - LeetSync

---
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] findFarmland(int[][] land) {
+        ArrayList<int[]> ans = new ArrayList<>();
+        for(int i = 0 ; i < land.length; i++){
+            for(int j = 0; j <land[0].length; j++){
+                if(land[i][j] == 0 || i > 0 && land[i-1][j] == 1 || j > 0 && land[i][j-1] == 1){ //if not top left corner will skipp
+                    continue;
+                }
+
+                int endRow = i;//dont add first to pass one hectre edge case
+                while(endRow + 1 < land.length && land[endRow + 1][j] == 1){
+                    endRow++;
+                }
+
+                int endColumn = j;
+                while(endColumn + 1 < land[0].length && land[i][endColumn + 1] == 1){
+                    endColumn++;
+                }
+
+                ans.add(new int[]{i,j,endRow,endColumn});
+            }
+        }
+
+        return ans.toArray(new int[0][]);
+    }
+}
