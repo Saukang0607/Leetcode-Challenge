7c58065c1cf316bb3753c451f7cc57bcc2f80969
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-05-05 11:29:53 +0800

    Time: 0 ms (100.00%) | Memory: 7.5 MB (28.35%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -3,6 +3,7 @@ public:
     string reversePrefix(string word, char ch) {
         int index = word.find(ch);
         cout << index;
+        cout << string::npos;
         reverse(word.begin(), word.begin() + index + 1); //not inclusive so +1 is required
         return word;
     }
