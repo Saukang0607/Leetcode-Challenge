1ec5f1e25db8b3214df09f61196a87cd528c3b2a
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-05-05 11:30:25 +0800

    Time: 0 ms (100.00%) | Memory: 7.4 MB (64.97%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string reversePrefix(string word, char ch) {
         int index = word.find(ch);
-        cout << index;
+        cout << index << "\n";
         cout << string::npos;
         reverse(word.begin(), word.begin() + index + 1); //not inclusive so +1 is required
         return word;
