88075ecd31288aabf41e6414853c0fb2c92dffed
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-05-07 23:45:09 +0800

    Time: 4 ms (77.89%) | Memory: 47.3 MB (6.99%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public ListNode doubleIt(ListNode head) {
-        if(carry(head)){ //still have the carry
+        if(carry(head)){ //still have the carry hence appending a new head with value 1
             ListNode newHead = new ListNode(1);
             newHead.next = head;
             return newHead;
@@ -21,22 +21,19 @@ class Solution {
     public boolean carry(ListNode curr){
         boolean plusOne;
         if(curr.next != null){
-            plusOne = carry(curr.next);
+            plusOne = carry(curr.next); //recursion is used to traverse the list backwards
         }
-        else{ //base case
+        else{ //base case: last node
             plusOne = (curr.val * 2 > 9) ? true : false; 
             curr.val = (curr.val * 2) % 10;
             return plusOne;
         }
 
-        boolean newPlusOne = (curr.val * 2 > 9) ? true : false;
+        boolean newPlusOne = (curr.val * 2 > 9) ? true : false; //doesn't matter got carry or not
+        curr.val = (curr.val * 2) % 10;
         if(plusOne){
-            curr.val = (curr.val * 2) % 10 + 1;
+            curr.val += 1;
         }
-        else{
-            curr.val = (curr.val * 2) % 10;
-        }
-
         return newPlusOne;
     }
 }
