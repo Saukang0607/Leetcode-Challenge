4395dc4887489448aa0bbd1e15d21166fdd5ab07
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-05-31 01:24:50 +0800

    Time: 26 ms (18.46%) | Memory: 41.2 MB (32.82%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.java
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.java
@@ -1,10 +1,10 @@
-class Solution {
+class Solution { //I didn't use hash table but you could use it to reduce time complexity
     public int countTriplets(int[] arr) {
         int n = arr.length;
         int ans = 0;
         int[] prefixXor = new int[n+1];
         for(int i = 0; i < n; i++){
-            prefixXor[i+1] = prefixXor[i]^arr[i];
+            prefixXor[i+1] = prefixXor[i]^arr[i]; //store the xor result of arr[0] to arr[i]
         }
 
         for(int i = 1; i <=n; i++){
