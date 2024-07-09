54cd3d72c67500bf0790926865b2d5980bcb40c9
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-07-09 20:42:22 +0800

    Time: 3 ms (93.06%) | Memory: 72.8 MB (32.65%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.java
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -1,12 +1,14 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-        double prev = customers[0][0] + customers[0][1], sum = customers[0][1];
+        double prev = customers[0][0] + customers[0][1], sum = customers[0][1]; //use double to prevent overflow
+        //prev is previous order done time
+
         for(int i = 1; i < customers.length; i++){
-            if(prev <= customers[i][0]){
+            if(prev <= customers[i][0]){ //if previous order is done before next one
                 prev = customers[i][0] + customers[i][1];
                 sum += customers[i][1];
             }
-            else{
+            else{ //still cooking previous order wehn getting new order
                 prev += customers[i][1];
                 sum += (prev - customers[i][0]);
             }
