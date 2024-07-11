ccc12e370d83d61c563d2cc51b1e88a37a4be376
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-07-11 22:07:17 +0800

    Time: 2 ms (70.03%) | Memory: 40.9 MB (99.14%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == '('){
                 stack.push(sb.toString());
-                sb.setLength(0);
+                sb = new StringBuffer();
             }
             else if(s.charAt(i) == ')'){
                 sb.reverse();
