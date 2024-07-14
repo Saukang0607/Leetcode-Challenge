5d408fc9cc6347df62c12eb8c3a1d160e1bf68b6
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-07-14 08:21:39 +0800

    Time: 989 ms (52.14%) | Memory: 47.6 MB (25.64%) - LeetSync

--- 2846-robot-collisions/robot-collisions.py
+++ 2846-robot-collisions/robot-collisions.py
@@ -1,25 +1,26 @@
 class Solution:
     def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
         n = len(positions)
-        bots_info = sorted(zip(range(n), positions, healths, directions), key = lambda self:self[1])
+        bots_info = sorted(zip(range(n), positions, healths, directions), key = lambda self_:self_[1])
         to_left = []
         to_right = [] 
         for i, p, h, d in bots_info:
             if d == "R":
                 to_right.append([i,h])
                 continue
-            while to_right: #collision happened, left bot as candidate
-                left = to_right[-1]
-                if left[1] > h:
-                    left[1] -= 1 
+            while to_right: #encountered bot to left, collision happened
+                rightmost = to_right[-1]
+                if rightmost[1] > h:
+                    rightmost[1] -= 1 
+                    #no need to pop the bot heading left since we never append it before
                     break
-                if left[1] == h:
+                elif rightmost[1] == h:
                     to_right.pop()
                     break
                 h -= 1
                 to_right.pop()
-            else:
+            else: #bots to right are all collided by a large health bot (loop didn't break)
                 to_left.append([i,h])
-        to_right += to_left
-        to_right.sort(key = lambda self:self[0])
+        to_right += to_left #at the end all bots will head toward same direction
+        to_right.sort(key = lambda self_:self_[0])
         return [h for idx, h in to_right]
