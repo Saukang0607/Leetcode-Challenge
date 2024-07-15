ee2641babc19ddc5090c70c7b05f083c699422b0
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-07-15 08:20:19 +0800

    Time: 28 ms (93.44%) | Memory: 16.8 MB (9.37%) - LeetSync

--- 726-number-of-atoms/number-of-atoms.py
+++ 726-number-of-atoms/number-of-atoms.py
@@ -2,60 +2,58 @@ class Solution:
     def countOfAtoms(self, formula: str) -> str:
         n = len(formula)
         map = defaultdict(lambda: 1)
-        d = deque([])
+        s = []
         i = idx = 0
         while i < n:
             c = formula[i]
-            if c == '(' or c == ')':
-                d.append(c)
+            if c =='(' or c == ')':
+                s.append(c)
                 i += 1
-            else:
-                if str.isdigit(c):
-                    # 获取完整的数字，并解析出对应的数值
-                    j = i
-                    while j < n and str.isdigit(formula[j]):
-                        j += 1
-                    cnt = int(formula[i:j])
-                    i = j
-                    # 如果栈顶元素是 )，说明当前数值可以应用给「连续一段」的原子中
-                    if d and d[-1] == ')':
-                        tmp = []
-                        d.pop()
-                        while d and d[-1] != '(':
-                            cur = d.pop()
-                            map[cur] *= cnt
-                            tmp.append(cur)
-                        d.pop()
-
-                        for k in range(len(tmp) - 1, -1, -1):
-                            d.append(tmp[k])
-                    # 如果栈顶元素不是 )，说明当前数值只能应用给栈顶的原子
-                    else:
-                        cur = d.pop()
+            elif str.isdigit(c):
+                j = i
+                while j < n and str.isdigit(formula[j]): 
+                    j+=1 #get the whole number
+                cnt = int(formula[i:j])
+                i = j
+                
+                if s and s[-1] == ')': #the number behind applies to all elements inside
+                    tmp = [] #to maintain the elements of stack
+                    s.pop()
+                    while s and s[-1] != '(':
+                        cur = s.pop()
                         map[cur] *= cnt
-                        d.append(cur)
-                else:
-                    # 获取完整的原子名
-                    j = i + 1
-                    while j < n and str.islower(formula[j]):
-                        j += 1
-                    cur = formula[i:j] + "_" + str(idx)
-                    idx += 1
-                    map[cur] = 1
-                    i = j
-                    d.append(cur)
+                        tmp.append(cur)
+                    s.pop()
 
-        #  将不同编号的相同原子进行合并
-        mm = defaultdict(int)
+                    for k in range(len(tmp) - 1, -1, -1): 
+                        s.append(tmp[k])
+                else: #single atom followed by a number case
+                    cur = s.pop()
+                    map[cur] *= cnt
+                    s.append(cur)
+
+            else: #get the full atom name
+                j = i + 1 #must start with capital letter
+                if j < n and str.islower(formula[j]):
+                    j += 1
+                cur = formula[i:j] + "_" + str(idx) #tips in handling atom in different position
+                idx += 1
+                map[cur] = 1
+                i = j
+                s.append(cur)
+
+        print(map)
+        combined_map = defaultdict(lambda: 0)
         for key, cnt in map.items():
             atom = key.split("_")[0]
-            mm[atom] += cnt
-
-        # 对mm中的key进行排序作为答案
+            combined_map[atom] += cnt
+        
         ans = []
-        for key in sorted(mm.keys()):
-            if mm[key] > 1:
-                ans.append(key+str(mm[key]))
+        for key in sorted(combined_map.keys()):
+            cnt = combined_map[key]
+            if cnt > 1:
+                ans.append(key + str(cnt))
             else:
                 ans.append(key)
         return "".join(ans)
+
