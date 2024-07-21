b94266afedbe74eec2f50516e44c75c050a8b7e8
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-07-22 07:55:41 +0800

    Time: 588 ms (43.57%) | Memory: 25.5 MB (47.14%) - LeetSync

--- 2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.py
+++ 2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.py
@@ -1,27 +1,34 @@
 class Solution:
     def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
         def topo_sort(edges: List[List[int]]) -> List[int]:
-            g = [[] for _ in range(k)]
+            out_deg_set = [[] for _ in range(k)]
             in_deg = [0] * k
-            for x, y in edges:
-                g[x - 1].append(y - 1)  # 顶点编号从 0 开始，方便计算
-                in_deg[y - 1] += 1
+            for x,y in edges:
+                out_deg_set[x-1].append(y-1)
+                in_deg[y-1] += 1
             order = []
-            q = deque(i for i, d in enumerate(in_deg) if d == 0)
-            while q:
-                x = q.popleft()
-                order.append(x)
-                for y in g[x]:
-                    in_deg[y] -= 1
-                    if in_deg[y] == 0:
-                        q.append(y)
-            return order if len(order) == k else None
-
-        if (row := topo_sort(rowConditions)) is None or (col := topo_sort(colConditions)) is None:
+            queue = deque(i for i, _ in enumerate(in_deg) if _ == 0) #the elements that will stay on top/left
+            while queue:
+                element = queue.popleft()
+                order.append(element)
+                for out in out_deg_set[element]:
+                    in_deg[out] -= 1
+                    if in_deg[out] == 0:
+                        queue.append(out)
+            
+            if len(order) == k:
+                return order
+            return None
+        
+        row_order = topo_sort(rowConditions)
+        col_order = topo_sort(colConditions)
+        if row_order is None or col_order is None: #there is a cycle in topological order, answer didn't exist
             return []
-        pos = {x: i for i, x in enumerate(col)}
+        pos = {x:i for i, x in enumerate(col_order)} #dictionary of value:index
         ans = [[0] * k for _ in range(k)]
-        for i, x in enumerate(row):
-            ans[i][pos[x]] = x + 1
+        for i, x in enumerate(row_order):
+            ans[i][pos[x]] = x+1
         return ans
 
+
+        
