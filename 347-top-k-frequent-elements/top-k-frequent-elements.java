2a9e4d387fa7e623d1a41c58272a72af312b1907
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-07-22 17:44:27 +0800

    Time: 31 ms (5.03%) | Memory: 48.4 MB (56.33%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,32 +1,25 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        //设置一个map集合,key存放数字,value存放出现次数
-        Map<Integer,Integer> map = new HashMap<>();
-        //统计出现次数
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        //建立一个大根堆,用来存放key值,堆内元素按照key对应的value值从大到小排序
-        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>(){
-            public int compare(Integer a,Integer b){
-                return map.get(a) - map.get(b);
-            }
-        });
-        //将map中的数字,插入到大根堆中
-        for(Integer key:map.keySet()){
-            if(queue.size() < k){
-                queue.add(key);
-            }else if(map.get(key) > map.get(queue.peek())){
+
+        Queue<Integer> queue = new PriorityQueue<>(Comparator.comparing(map::get));
+
+        for (Integer num : map.keySet()) {
+            queue.add(num);
+            if (queue.size() > k) {
                 queue.poll();
-                queue.add(key);
             }
         }
-        //将大根堆中的k个数字放到数组中
-        int [] res = new int[k];
-        int index = 0;
-        while(!queue.isEmpty()){
-            res[index++] = queue.poll();
+
+        int[] result = new int[k];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = queue.poll();
         }
-        return res;
+        return result;
     }
 }
