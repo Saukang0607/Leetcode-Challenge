eac3c28a5650373da4b4972ec15d6ef47db2265d
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-07-25 08:05:08 +0800

    Time: 106 ms (73.87%) | Memory: 55.2 MB (93.69%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] sortJumbled(int[] mapping, int[] nums) {
-        int mapped[] = new int[nums.length];
-        Integer[] idx = new Integer[nums.length];
+        int mapped[] = new int[nums.length]; //stores mapped version nums
+        Integer[] idx = new Integer[nums.length]; //stores sorted mapped indexes
 
         for(int i = 0; i < nums.length; i++){
             String numberString = Integer.toString(nums[i]);
             int rebuiltNumber = 0;
-            for (int j = 0; j < numberString.length(); j++) {
+            for (int j = 0; j < numberString.length(); j++) { //mapping nums[i] digit one by one
                 char digitChar = numberString.charAt(j);
                 int index = Character.getNumericValue(digitChar);
                 rebuiltNumber = rebuiltNumber * 10 + mapping[index];
@@ -15,11 +15,9 @@ class Solution {
             idx[i] = i;
         }
 
-        Arrays.sort(idx, (i, j) -> mapped[i] - mapped[j]);
-        System.out.println(Arrays.toString(mapped));
-        System.out.println(Arrays.toString(idx));
+        Arrays.sort(idx, (i, j) -> mapped[i] - mapped[j]); //sort indexes based on non-decreasing order of mapped
 
-        int[] ans = new int[nums.length];
+        int[] ans = new int[nums.length]; //create new answer array to prevent overwrite
         for (int i = 0; i < nums.length; i++) {
             ans[i] = nums[idx[i]];
         }
