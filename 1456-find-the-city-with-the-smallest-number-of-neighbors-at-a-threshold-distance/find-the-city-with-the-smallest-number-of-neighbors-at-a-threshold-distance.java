e0a41657f141797fc83b8ba23903672921b414ba
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-07-27 10:47:31 +0800

    Time: 41 ms (20.34%) | Memory: 50.8 MB (5.65%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,40 +1,39 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int[][] w = new int[n][n];
-        for (int[] row : w) {
-            Arrays.fill(row, Integer.MAX_VALUE / 2); // 防止加法溢出
+        int[][] w = new int[n][n]; //total weight from i to j
+        for(int[] row : w){
+            Arrays.fill(row, Integer.MAX_VALUE / 2);
         }
-        for (int[] e : edges) {
-            int x = e[0], y = e[1], wt = e[2];
-            w[x][y] = w[y][x] = wt;
-        }
-        int[][][] memo = new int[n][n][n];
 
+        for(int[] edge : edges){
+            int x = edge[0], y = edge[1], weight = edge[2];
+            w[x][y] = w[y][x] = weight;
+        }
+
+        int[][][] memo = new int[n][n][n];
         int ans = 0;
-        int minCnt = n;
-        for (int i = 0; i < n; i++) {
+        int min_count = n;
+
+        for(int i = 0; i < n; i++){
             int cnt = 0;
-            for (int j = 0; j < n; j++) {
-                if (j != i && dfs(n - 1, i, j, memo, w) <= distanceThreshold) {
+            for(int j = 0; j < n; j++){
+                if(i != j && dfs(n-1, i, j, memo, w) <= distanceThreshold){
                     cnt++;
                 }
             }
-            if (cnt <= minCnt) { // 相等时取最大的 i
-                minCnt = cnt;
-                ans = i;
+            
+            if(cnt <= min_count){ //return city with greatest number 
+                min_count = cnt;
+                ans =  i;
             }
         }
+
         return ans;
     }
 
-    private int dfs(int k, int i, int j, int[][][] memo, int[][] w) {
-        if (k < 0) { // 递归边界
-            return w[i][j];
-        }
-        if (memo[k][i][j] != 0) { // 之前计算过
-            return memo[k][i][j];
-        }
-        return memo[k][i][j] = Math.min(dfs(k - 1, i, j, memo, w),
-                dfs(k - 1, i, k, memo, w) + dfs(k - 1, k, j, memo, w));
+    private int dfs(int k, int i, int j, int[][][] memo, int[][]w){
+        if(k < 0) return w[i][j]; //base case
+        if(memo[k][i][j] != 0) return memo[k][i][j];
+        return memo[k][i][j] = Math.min(dfs(k-1, i, j, memo, w), dfs(k-1, i, k, memo, w) + dfs(k-1, k, j, memo, w));
     }
-}   
+}
