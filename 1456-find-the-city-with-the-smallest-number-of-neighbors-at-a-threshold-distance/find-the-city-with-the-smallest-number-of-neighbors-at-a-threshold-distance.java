2b692e68f05130bc5e4efc8f3850efe5ba13d9c8
Lo Sau Kang <118031884+Saukang0607@users.noreply.github.com>
2024-07-27 11:03:12 +0800

    Time: 42 ms (19.99%) | Memory: 50.4 MB (5.65%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,8 +1,8 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int[][] w = new int[n][n]; //total weight from i to j
+        int[][] w = new int[n][n]; //total weights from city i to city j
         for(int[] row : w){
-            Arrays.fill(row, 10001);
+            Arrays.fill(row, 10001); //since weighti, distanceThreshold <= 10^4
         }
 
         for(int[] edge : edges){
@@ -10,9 +10,9 @@ class Solution {
             w[x][y] = w[y][x] = weight;
         }
 
-        int[][][] memo = new int[n][n][n];
+        int[][][] memo = new int[n][n][n]; //for memoization search
         int ans = 0;
-        int min_count = n - 1;
+        int min_count = n - 1; 
 
         for(int i = 0; i < n; i++){
             int cnt = 0;
@@ -35,5 +35,6 @@ class Solution {
         if(k < 0) return w[i][j]; //base case
         if(memo[k][i][j] != 0) return memo[k][i][j];
         return memo[k][i][j] = Math.min(dfs(k-1, i, j, memo, w), dfs(k-1, i, k, memo, w) + dfs(k-1, k, j, memo, w));
+        //state transition equation: indicates minimum weights path from city i to city j, with intermediate city's number less than k
     }
 }
